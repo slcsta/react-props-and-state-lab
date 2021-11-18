77e108ee02b96eeb6487f77ae97b4f0fea503102
Sarah <sarahlchavez@gmail.com>
2021-11-18 05:09:49 -0800

    commit tree 61ae9fb57a25e4cd692eeef2ae0707b2bbaf0387

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import Filters from './Filters'
 import PetBrowser from './PetBrowser'
 
@@ -15,6 +14,43 @@ class App extends React.Component {
     }
   }
 
+  fetchPets = () => {
+    let url = '/api/pets';
+      if (this.state.filters.type !== 'all') {
+        url += `?type=${this.state.filters.type}`
+      }
+  
+      fetch(url)
+        .then(response => response.json())
+        .then(pets => this.setState({ pets: pets }));
+  }
+  
+
+
+  // then we create a function up here called onChangeType
+  // no event handlers right now
+  handleChangeFilterType = (e) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: e.target.value
+      }
+    })
+
+  }
+  // when the filters component calls onFindPetsClick should fetch list of
+  // pets using fetch - when we fetch we want to change the state of this
+  // use filter type to fetch pets and put them into the pets array
+  // call this base url if type is all but call api with a query parameter 
+  //if the type is not all, but instead one of the animal types
+  
+  onAdoptPet = (petId) => {
+    const pets = this.state.pets.map(p => {
+      return p.id === petId ? { ...p, isAdopted: true } : p;
+    })
+    this.setState({ pets: pets })
+  }
+  // told we need 2 callbacks for filters: onChangeType and onFindPetsClick
   render() {
     return (
       <div className="ui container">
@@ -24,10 +60,11 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeFilterType}
+              onFindPetsClick={this.fetchPets} />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
@@ -35,5 +72,10 @@ class App extends React.Component {
     )
   }
 }
+
+// app should pass a callback prop onChangeType to <Filters />
+// and this callback needs to update App's state
+// because it's a callback we know it will be a function {}
+// should have an onFindPetsClick callback prop
 
 export default App
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
+// we want to connect the value selected to our state
 class Filters extends React.Component {
+  
+  handleChange = e => {
+    this.props.onChangeType(e.target.value)
+  }
+  
   render() {
     return (
       <div className="ui form">
         <h3>Animal type</h3>
         <div className="field">
-          <select name="type" id="type">
+          <select name="type" id="type" value={this.props.filters.type}
+          onChange={this.props.onChangeType}>
             <option value="all">All</option>
             <option value="cat">Cats</option>
             <option value="dog">Dogs</option>
@@ -15,7 +22,8 @@ class Filters extends React.Component {
         </div>
 
         <div className="field">
-          <button className="ui secondary button">Find pets</button>
+          <button className="ui secondary button" 
+          onClick={this.props.onFindPetsClick}>Find pets</button>
         </div>
       </div>
     )
